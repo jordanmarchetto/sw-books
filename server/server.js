81ea44450e93a52583213a91f7a77056a053893f
Jordan Marchetto <jordan.marchetto@ncr.com>
2021-08-06 14:23:55 -0400

    fix envs and configs

--- server/server.js
+++ server/server.js
@@ -9,15 +9,6 @@ const DB = { BOOKS: [{ title: "book 1", author: "author lname", completed: true,
 //table to reference in db
 const TABLE_NAME = "books";
 
-//set up db connector
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '', // Password is empty be default
-    port: 5432, // Default port
-});
-
 //setup server/router
 const server = new Koa();
 const router = new KoaRouter();
@@ -29,12 +20,33 @@ server.use(bodyParser());
 //router middleware, allow routes
 server.use(router.routes()).use(router.allowedMethods());
 
-  
+//set up db connector
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: 'postgres',
+    database: 'postgres',
+    password: 'postgres', // Password is empty be default
+});
+
+//make sure we're connected to the db
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release()
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+        console.log(result.rows)
+    })
+})
+
 
 //get all books from the db
 const getAllBooks = async (ctx) => {
-    //ctx.body = DB.BOOKS;
-    const { rows } = await pool.query(`SELECT * FROM ${TABLE_NAME};`)
+    const { rows } = await pool.query(`SELECT * FROM ${TABLE_NAME};`).catch(e => { console.error(e) })
     ctx.body = rows;
 }
 
@@ -53,9 +65,9 @@ const deleteBook = async (book_id) => {
 //add a single book to the db
 const addBook = async (ctx) => {
     const body = ctx.request.body;
-    const {title, author, completed, rating} = body;
+    const { title, author, completed, rating } = body;
     const query = `INSERT INTO ${TABLE_NAME} (title, author, completed, rating) VALUES ('${title}', '${author}', '${completed}', '${rating}') RETURNING book_id;`;
-    const book_id = await pool.query(query).then(res=> res.rows[0].book_id);
+    const book_id = await pool.query(query).then(res => res.rows[0].book_id);
     ctx.body = await getBook(book_id);
 }
 
@@ -63,7 +75,7 @@ const addBook = async (ctx) => {
 //expects all fields/columns to be populated
 const updateBook = async (ctx) => {
     const body = ctx.request.body;
-    const {title, author, completed, rating} = body;
+    const { title, author, completed, rating } = body;
     const book_id = ctx.params.book_id;
     const query = `UPDATE ${TABLE_NAME} SET title = '${title}', author = '${author}', completed = '${completed}', rating = '${rating}' WHERE book_id = '${book_id}'`;
     await pool.query(query);
@@ -71,12 +83,12 @@ const updateBook = async (ctx) => {
 }
 
 //define routes
-router.get("/books",  getAllBooks);
+router.get("/books", getAllBooks);
 router.get("/books/:book_id", async (ctx) => ctx.body = await getBook(ctx.params.book_id));
 router.put("/books/:book_id", updateBook);
 router.delete("/books/:book_id", async (ctx) => ctx.body = await deleteBook(ctx.params.book_id));
 router.post("/books", addBook);
 
 //start server
-const port = process.env.PORT || 2222;
+const port = process.env.SERVER_PORT || 2222;
 server.listen(port, () => console.log(`server listening on http://localhost:${port}`));
